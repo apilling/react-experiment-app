48353e037398198c28586a63d0110833ae7b6fd5
Alex Pilling <apilling@udel.edu>
2021-09-21 16:57:50 -0400

    Added initial card data and CardViewer/ControlPanel layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,16 @@
 import React, { useState } from 'react';
 import './App.css';
-import {Prompt} from './components/prompt';
-import { SwapButton } from './components/swapButton';
+import CARDS from "./assets/cards.json";
+import { CardViewer } from './components/CardViewer';
+import { ControlPanel } from './components/ControlPanel';
 
 
 function App(): JSX.Element {
-  const [prompt, setPrompt] = useState<string>("Hello and Welcome!");
   return (
     <div className="App">
       <div>
-          <Prompt text={prompt}></Prompt>
-          <SwapButton change={setPrompt}></SwapButton>
+          <ControlPanel></ControlPanel>
+          <CardViewer></CardViewer>
       </div>
     </div>
   );
